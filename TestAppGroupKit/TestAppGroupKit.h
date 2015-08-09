2d4d10a1c4fab68c5804799476cb050fe45d5078
Christopher Stott <christopherstott@gmail.com>
2015-08-08 17:28:52 -0700

    Initial

---
+++ TestAppGroupKit/TestAppGroupKit.h
@@ -0,0 +1,19 @@
+//
+//  TestAppGroupKit.h
+//  TestAppGroupKit
+//
+//  Created by Chris on 2015-08-08.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestAppGroupKit.
+FOUNDATION_EXPORT double TestAppGroupKitVersionNumber;
+
+//! Project version string for TestAppGroupKit.
+FOUNDATION_EXPORT const unsigned char TestAppGroupKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestAppGroupKit/PublicHeader.h>
+
+
